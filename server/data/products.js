cbdc43ab6a81b144e17c9f89c2798af1f79d23d3
pedroe.ramos <pedroe.ramos@softtek.com>
2021-03-15 17:39:05 -0600

    first commit

---
+++ server/data/products.js
@@ -0,0 +1,58 @@
+const products = [
+	{
+		name: 'PlayStation 5',
+		imageUrl:
+			'https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80',
+		description:
+			'PlayStation 5 (PS5) es una consola de videojuegos para el hogar desarrollada por Sony Interactive Entertainment. Anunciada en 2019 como la sucesora de PlayStation 4, la PS5 se lanzó el 12 de noviembre de 2020 en Australia, Japón, Nueva Zelanda, América del Norte, Singapur y Corea del Sur, y el 19 de noviembre de 2020 en adelante en otros mercados importantes excepto China y India.',
+		price: 499,
+		countInStock: 15
+	},
+	{
+		name: 'Iphone 12',
+		imageUrl:
+			'https://images.unsplash.com/photo-1605787020600-b9ebd5df1d07?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1463&q=80',
+		description:
+			'Bienvenido a una nueva era de iPhone. Pantalla Super Retina XDR de 6,1 pulgadas maravillosamente brillante.1 Ceramic Shield con un rendimiento de caída 4 veces mejor.2 Increíble fotografía con poca luz con modo Noche en todas las cámaras. Grabación, edición y reproducción de video Dolby Vision de calidad cinematográfica. Potente chip biónico A14. Y nuevos accesorios MagSafe para una conexión fácil y una carga inalámbrica más rápida.3 Que comience la diversión.',
+		price: 1099,
+		countInStock: 10
+	},
+	{
+		name: 'Cannon EOS-1D',
+		imageUrl:
+			'https://images.unsplash.com/photo-1519183071298-a2962feb14f4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+		description:
+			'La EOS-1D X combina velocidad con calidad de imagen para crear la cámara de próxima generación para profesionales. El sensor de fotograma completo de 18 megapíxeles con procesadores duales “DIGIC 5+” establece el estándar, y la captura de hasta 12 fotogramas por segundo lo lleva más allá.',
+		price: 1300,
+		countInStock: 5
+	},
+	{
+		name: 'Amazon Alexa',
+		imageUrl:
+			'https://images.unsplash.com/photo-1518444065439-e933c06ce9cd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80',
+		description:
+			'Es capaz de interactuar por voz, reproducir música, hacer listas de tareas, configurar alarmas, transmitir podcasts, reproducir audiolibros y proporcionar información meteorológica, de tráfico, deportes y otra información en tiempo real, como noticias. Alexa también puede controlar varios dispositivos inteligentes utilizándose a sí misma como un sistema de automatización del hogar.',
+		price: 50,
+		countInStock: 25
+	},
+	{
+		name: 'Audio Technica Headphones',
+		imageUrl:
+			'https://images.unsplash.com/photo-1558756520-22cfe5d382ca?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+		description:
+			'Equipados con controladores dinámicos de gran apertura de 45 mm y un diseño cerrado y sobre la oreja, los auriculares ATH-M50x brindan claridad, bajos profundos y ancho de banda extendido (15 Hz a 28 kHz) mientras lo aíslan de los sonidos externos.',
+		price: 233,
+		countInStock: 4
+	},
+	{
+		name: 'JBL FLIP 4',
+		imageUrl:
+			'https://images.unsplash.com/photo-1564424224827-cd24b8915874?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80',
+		description:
+			'JBL Flip 4 es la próxima generación de la galardonada serie Flip; es un altavoz Bluetooth portátil que ofrece un sonido estéreo sorprendentemente potente. Este altavoz compacto funciona con una batería de iones de litio recargable de 3000 mAh que ofrece hasta 12 horas de reproducción de audio continua y de alta calidad.',
+		price: 140,
+		countInStock: 10
+	}
+]
+
+module.exports = products
